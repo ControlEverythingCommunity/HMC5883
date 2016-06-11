7b62fac2af565d190112741a49d64ed1b4ba2ddc
Yadwinder Singh <ryker1990@gmail.com>
2016-06-12 01:49:10 +0530

    Update HMC5883.ino

--- Arduino/HMC5883.ino
+++ Arduino/HMC5883.ino
@@ -11,55 +11,55 @@
 
 void setup()
 {
-	// Initialise I2C communication as MASTER
-	Wire.begin();
-	// Initialise Serial Communication, set baud rate = 9600
-	Serial.begin(9600);
-	
-	// Start I2C Transmission
-	Wire.beginTransmission(Addr);
-	// Select configure register A
-	Wire.write(0x00);
-	// Set normal measurement configuration, data output rate = 0.75Hz
-	Wire.write(0x60);
-	// Stop I2C Transmission
-	Wire.endTransmission();
+  // Initialise I2C communication as MASTER
+  Wire.begin();
+  // Initialise Serial Communication, set baud rate = 9600
+  Serial.begin(9600);
+  
+  // Start I2C Transmission
+  Wire.beginTransmission(Addr);
+  // Select configure register A
+  Wire.write(0x00);
+  // Set normal measurement configuration, data output rate = 0.75Hz
+  Wire.write(0x60);
+  // Stop I2C Transmission
+  Wire.endTransmission();
 
-	// Start I2C Transmission
-	Wire.beginTransmission(Addr);
-	// Select Mode register
-	Wire.write(0x02);
-	// Set continuous measurement
-	Wire.write(0x00);
-	// Stop I2C Transmission
-	Wire.endTransmission();
-	delay(300);
+  // Start I2C Transmission
+  Wire.beginTransmission(Addr);
+  // Select Mode register
+  Wire.write(0x02);
+  // Set continuous measurement
+  Wire.write(0x00);
+  // Stop I2C Transmission
+  Wire.endTransmission();
+  delay(300);
 }
 
 void loop()
 {
   unsigned int data[6];
   // Start I2C Transmission
-	Wire.beginTransmission(Addr);
+  Wire.beginTransmission(Addr);
   // Select data register
-	Wire.write(0x03);
-	// Stop I2C Transmission
-	Wire.endTransmission();
+  Wire.write(0x03);
+  // Stop I2C Transmission
+  Wire.endTransmission();
 
-	// Request 6 bytes of data
+  // Request 6 bytes of data
   Wire.requestFrom(Addr, 6);
   
   // Read 6 bytes of data
   // xMag msb, xMag lsb, zMag msb, zMag lsb, yMag msb, yMag lsb
-	if(Wire.available() == 6)
-	{
+  if(Wire.available() == 6)
+  {
     data[0] = Wire.read();
     data[1] = Wire.read();
     data[2] = Wire.read();
     data[3] = Wire.read();
     data[4] = Wire.read();
     data[5] = Wire.read();
-	}
+  }
   delay(300);
         
   // Convert the data 
